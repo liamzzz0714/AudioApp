cd817c0229f170b13dbe271d04be87e56a58df49
liamzzz0714 <liamzzz0714@gmail.com>
2020-08-22 16:20:35 +0800

    deploy

---
+++ README.md
@@ -0,0 +1,8 @@
+# 音樂播放程式
+
+[作品連結](https://liamzzz0714.github.io/AudioApp/)
+
+- 使用 React 進行開發
+- context API 與 React Hook 做狀態管理
+- indexedDB 播放清單儲存
+- service-worker 快取，支援離線使用
---
+++ service-worker.js
@@ -0,0 +1,47 @@
+"use strict";
+const CACHE_NAME = "static-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "bundle.js",
+  "icon-512.png"
+];
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.mode !== "navigate") {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.open(CACHE_NAME).then(cache => {
+        return cache.match("index.html");
+      });
+    })
+  );
+});
